e0146490f15b80ed3ccb9b663ddb87ede4bf7654
Carla Drieli <carladrieli@gmail.com>
2015-10-28 11:32:22 -0300

    incluindo bibliotecas

--- src/oportunidades/model/Empresa.java
+++ src/oportunidades/model/Empresa.java
@@ -3,7 +3,6 @@ package oportunidades.model;
 public class Empresa {
 	
 	private Usuario usuario;
-	private Usuario empresa;
 	private String endereco;
 
 	public String getEndereco() {
@@ -12,14 +11,6 @@ public class Empresa {
 
 	public void setEndereco(String endereco) {
 		this.endereco = endereco;
-	}
-
-	public Usuario getEmpresa() {
-		return empresa;
-	}
-
-	public void setEmpresa(Usuario empresa) {
-		this.empresa = empresa;
 	}
 
 	public Usuario getUsuario() {
