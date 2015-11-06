7bc42c0d0771f1e60bcec3595e482ab4cf65970c
Arley Prates <arleyprates@gmail.com>
2015-11-06 12:59:12 -0300

    Ajuste em insereAluno de AlunoDAO com matricula e insere Usuario antes de inserir Aluno.

--- src/oportunidades/dao/AlunoDAO.java
+++ src/oportunidades/dao/AlunoDAO.java
@@ -4,6 +4,7 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 
 import oportunidades.model.Aluno;
+import oportunidades.model.Usuario;
 
 public class AlunoDAO {
 
@@ -16,40 +17,52 @@ public class AlunoDAO {
 
 		} catch (Exception e) {
 			// TODO Auto-generated catch block
-			System.out.println("Exceção ao criar Conexao.");
+			System.out.println("Excecao ao criar Conexao.");
 			e.printStackTrace();
 		}
 
 	}
 
 	public void insereAluno(Aluno aluno) {
-
+		
 		String sql = "INSERT INTO aluno"
-				+ "(idusuario, semestre, previsao_conclusao, idcurso)"
-				+ " values (?,?,?,?)";
+				+ "(idusuario, semestre, matricula, previsaoConclusao, idcurso)"
+				+ " values (?,?,?,?,?)";
 
 		PreparedStatement stmt = null;
+		Usuario aux = null;
 
 		try {
+			System.out.println("Inserindo aluno");
 			if (connection == null) {
 				System.out.println("conexao null");
 				this.connection = new ConexaoDAO().getConexao();
 			}
-
+			
+			UsuarioDAO usuarioDAO = new UsuarioDAO();
+			aluno.setTipo(1);
+			System.out.println("Inserindo Usuario");
+			usuarioDAO.insereUsuario(aluno);
+			
+			aux = usuarioDAO.buscaUsuarioId(aluno);
+			
 			stmt = connection.prepareStatement(sql);
-			System.out.println("Inserindo aluno");
-			stmt.setInt(1, aluno.getUsuario().getId());
+			
+			stmt.setInt(1, aux.getId());
 			stmt.setString(2, aluno.getSemestre());
-			stmt.setString(3, aluno.getPrevisao_conclusao());
-			stmt.setInt(4, aluno.getCurso().getId());
+			stmt.setString(3, aluno.getMatricula());
+			stmt.setString(4, aluno.getPrevisaoConclusao());
+			stmt.setInt(5, 4);
 
+			//stmt1.execute();
+			//stmt1.close();
 			stmt.execute();
 
 			stmt.close();
 
 		} catch (Exception e) {
 			// TODO: handle exception
-			System.out.println("Exceção ao inserir Aluno.");
+			System.out.println("Excecao ao inserir Aluno.");
 			e.printStackTrace();
 		}
 
