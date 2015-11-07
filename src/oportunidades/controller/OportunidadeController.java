fe7cc9f5ef6339bbe23e1e3b4fc70a373232ccc3
Carla Drieli <carladrieli@gmail.com>
2015-11-07 02:21:37 -0300

    Inserindo Oportunidade

--- src/oportunidades/controller/OportunidadeController.java
+++ src/oportunidades/controller/OportunidadeController.java
@@ -1,5 +1,28 @@
 package oportunidades.controller;
 
-public class OportunidadeController {
+import javax.faces.bean.ManagedBean;
 
+import oportunidades.dao.OportunidadeDAO;
+import oportunidades.model.Oportunidade;
+
+@ManagedBean (name = "oportunidadeController")
+public class OportunidadeController {		 
+
+		private Oportunidade oportunidade = new Oportunidade();
+
+		public Oportunidade getOportunidade() {
+			return oportunidade;
+		}
+		
+		public void setOportunidade(Oportunidade oportunidade){
+			this.oportunidade = oportunidade;
+		}
+			
+		
+		public String insereOportunidade() throws Exception{
+			OportunidadeDAO daoOportunidade = new OportunidadeDAO();
+			daoOportunidade.insereOportunidade(oportunidade);
+			return oportunidade.getNome();
+			
+		}
 }
