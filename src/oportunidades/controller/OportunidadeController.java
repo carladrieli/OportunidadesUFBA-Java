7600f45516c584ddcf8759101ec06a1c6cdbfd2b
Arley Prates <arleyprates@gmail.com>
2015-11-13 10:53:51 -0300

    ajustes

--- src/oportunidades/controller/OportunidadeController.java
+++ src/oportunidades/controller/OportunidadeController.java
@@ -3,16 +3,25 @@ package oportunidades.controller;
 import java.util.List;
 
 import javax.faces.bean.ManagedBean;
+import javax.faces.event.AjaxBehaviorEvent;
+
+import oportunidades.dao.CursoDAO;
 import oportunidades.dao.OportunidadeDAO;
+import oportunidades.model.Area;
+import oportunidades.model.Curso;
 import oportunidades.model.Oportunidade;
 
 @ManagedBean (name = "oportunidadeController")
 public class OportunidadeController {		 
 
 		private Oportunidade oportunidade = new Oportunidade();
+		private Curso curso = new Curso();
+		private Area area = new Area();
 		private List<Oportunidade> listaOportunidade = null;
+		private List<Curso> listaCurso = null;
 		private int areaId;
 
+		
 		public Oportunidade getOportunidade() {
 			return oportunidade;
 		}
@@ -34,6 +43,19 @@ public class OportunidadeController {
 			listaOportunidade = daoOportunidade.listaOportunidade();
 			return listaOportunidade;
 		}
+		
+		public List<Curso> buscaCursoArea() throws Exception {
+			CursoDAO daoCurso = new CursoDAO();
+			System.out.println(areaId);
+			listaCurso = daoCurso.buscaCursoArea(area.getId());
+			return listaCurso;
+		}
+		
+		public void listaCursos(AjaxBehaviorEvent event) {
+			CursoDAO daoCurso = new CursoDAO();
+			System.out.println("Listacurso Oportunidade");
+			//listaCurso = daoCurso.buscaCursoArea(curso.getArea().getId());
+		}
 
 		public int getAreaId() {
 			return areaId;
@@ -42,4 +64,38 @@ public class OportunidadeController {
 		public void setAreaId(int areaId) {
 			this.areaId = areaId;
 		}
+
+		public List<Curso> getListaCurso() {
+			return listaCurso;
+		}
+
+		public void setListaCurso(List<Curso> listaCurso) {
+			this.listaCurso = listaCurso;
+		}
+
+		public Curso getCurso() {
+			return curso;
+		}
+
+		public void setCurso(Curso curso) {
+			this.curso = curso;
+		}
+
+		public Area getArea() {
+			return area;
+		}
+
+		public void setArea(Area area) {
+			this.area = area;
+		}
+
+		public List<Oportunidade> getListaOportunidade() {
+			return listaOportunidade;
+		}
+
+		public void setListaOportunidade(List<Oportunidade> listaOportunidade) {
+			this.listaOportunidade = listaOportunidade;
+		}
+		
+		
 }
